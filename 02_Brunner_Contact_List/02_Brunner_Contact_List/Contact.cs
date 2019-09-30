84bdc6bb736fe5844659fa060be987a2b728cd80
BrunnerAlexander <AlexanderBrunner01@gmail.com>
2019-09-30 11:28:06 +0200

    Solution to the Homework

---
+++ 02_Brunner_Contact_List/02_Brunner_Contact_List/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_Brunner_Contact_List.Controllers
+{
+    public class Contact
+    {
+        public Int32 Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ 02_Brunner_Contact_List/02_Brunner_Contact_List/Controllers/ContactsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_Brunner_Contact_List.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private static List<Contact> contacts = new List<Contact> { };
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(contacts);
+        }
+
+
+        [HttpPost]
+        public IActionResult AddItem([FromBody] Contact contact)
+        {
+            contacts.Add(contact);
+            return CreatedAtRoute("GetSpecificContact", new { index = contacts.IndexOf(contact) }, contact);
+        }
+
+
+        [HttpDelete]
+        [Route("{index}")]
+        public IActionResult DeleteItem(int index)
+        {
+            if (index >= 0 && index < contacts.Count)
+            {
+                contacts.RemoveAt(index);
+                return NoContent();
+            }
+
+            return BadRequest("Invalid index");
+        }
+
+        [HttpGet]
+        [Route("findByName/{name}")]
+        public Contact AddItem(string name)
+        {
+            Contact ret = contacts.FirstOrDefault(c => c.FirstName.Contains(name) || c.LastName.Contains(name));
+            return ret;
+        }
+
+    }
+}
